379ace20159e7a0bb7ae872ec48e6c3ed1a210a4
ARMEL EUDES SAMADI <138219251+armeldevsarl@users.noreply.github.com>
2023-10-11 13:42:58 +0100

    Update retrive.sol

--- retrive.sol
+++ retrive.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
-contract  retrive {
-    uint256 private firstvalue;
 
+contract retrive {
+    uint256 private firstvalue; // Private state variable to store an unsigned integer.
 
-    function setFirstValue ( uint256  _neoValue) public {
-        firstvalue= _neoValue;
-    
-}
+    // Function to set the value of firstvalue.
+    function setFirstValue(uint256 _neoValue) public {
+        firstvalue = _neoValue; // Update firstvalue with the provided _neoValue.
+    }
+
+    // Function to retrieve the current value of firstvalue.
     function getFirstValue() public view returns (uint256) {
-        return firstvalue;
+        return firstvalue; // Return the current value of firstvalue.
     }
 }
-    
-
